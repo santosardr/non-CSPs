c4cf5753d84a62ac41b862ac7f74a8f8167d58b4
santosardr <santosardr@ufu.br>
2022-12-26 14:24:34 -0300

    DBScan with parameter 1.1 kept the maximum ACC. Removing a list of 168 negatives classified as NAs did the trick of increasing sensitivity.

--- src/DBScan.R
+++ src/DBScan.R
@@ -3,7 +3,7 @@ WPM("load-packages", "optics_dbScan")
 DBScan<-make_Weka_clusterer('weka/clusterers/DBSCAN', c("DBScan", "Weka_clusterers"), package = "optics_dbScan")
 data<-read.arff("training.arff")
 
-control<-c( "-E", 2, "-M", 6, "-A", "weka.core.EuclideanDistance", "--", "-R", "first-last" );
+control<-c( "-E", 1.1, "-M", 6, "-A", "weka.core.EuclideanDistance", "--", "-R", "first-last" );
 fullresult <- DBScan(data, control);
 table(predict(fullresult), data$class);
 result<-predict(fullresult);
@@ -16,10 +16,10 @@ length(as.numeric(labels(neg[which(is.na(neg))])));
 length(as.numeric(labels(pos[which(is.na(pos))])))
 
 printf <- function(...) cat(sprintf(...))
-lista=as.numeric(labels(neg[which(is.na(neg))]))+144+141
+lista=as.numeric(labels(neg[which(is.na(neg))]))+141
 for(item in lista){ printf("%dd;",  item) }
 
-lista=as.numeric(labels(neg[which(neg==1)]))+144+141;for(item in lista){ printf("%d ",  item) }
+lista=as.numeric(labels(neg[which(neg==1)]))+141;for(item in lista){ printf("%d ",  item) }
 i=0; preditos=(  ); for indice in ${preditos[@]}; do indice=$[$indice+$i]; sed -i "$[indice]d" filtered.arff; i=$[$i-1];done
 
 
