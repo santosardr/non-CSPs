8a8d737a7a140406bac6cb5ef575aa4f5708baca
Anderson Santos <santosardr@gmail.com>
2022-12-27 10:48:57 -0300

    A random procedure to exclude four proteins at once, retraining, and validating allowed me to accomplish 92 and 86% of specificity and sensitivity, respectively. I am running this algorithm recursively, like a genetic algorithm (semiag), over the resulting data.

---
+++ semiag/semiag4.bash
@@ -0,0 +1,65 @@
+#!/bin/bash -
+
+function randomgenerator()
+{
+    local rand=$(LC_ALL=C tr -dc '[:digit:]' < /dev/urandom|head -c8);
+    local real=$(echo "scale=8;1048*($rand/100000000)"|bc -l);
+    local integer=$(echo "scale=0;$real/1.0"|bc -l);
+    if [ $integer -eq 0 ];
+    then
+	integer=1
+    fi
+    echo "$integer"
+    }
+
+if [ $1 -gt 0 -a $2 -gt 0 ];
+then
+    for run in $(seq 1 $2);
+    do
+	if [ -r training.arff ];
+	then
+	    rand=$(randomgenerator)
+	    dela1=$[$rand  + 285 ];
+	    rand=$(randomgenerator)
+	    dela2=$[$dela1 + 1000];
+	    delc1=$[$rand  + 285 ];
+	    rand=$(randomgenerator)
+	    delb1=$[$rand  + 285 ];
+	    rand=$(randomgenerator)
+	    delb2=$[$delb1 + 1000];
+	    deld1=$[$rand  + 285 ];
+            rand=$(randomgenerator)
+            delc2=$[$rand + 1000];
+	    run=$(echo "$dela1$delb1$dela2$delb2$delc1$delc2")
+            printf  "\n%d,%d,%d,%d" $dela1 $delb1 $delc1 $deld1 >> group$1.result	    
+	    sed "$[$dela1]d;$[$delb1]d;$[$delc1]d;$[$deld1]d" training.arff > $run-training.arff
+		if [ -r $run-training.arff ];
+		then
+		    java -cp  /usr/local/weka/weka.jar  weka.classifiers.trees.RandomForest -t $run-training.arff -d $run-model.bin  > /dev/null 2>&1
+		    if [ -r $run-model.bin ];
+		    then
+			if [ -r validation1.arff ];
+			then
+			    java -cp  /usr/local/weka/weka.jar  weka.classifiers.trees.RandomForest -l $run-model.bin -p 0 -T validation1.arff > $run-teste1.result
+
+			    sed -i "1,5d;s/[ ]\+/ /g; s/^[ ]//g; s/[ ]$//g;  s/[ ]/\t/g" $run-teste?.result
+			    awk ' BEGIN{fp=0;fn=0;nn=0;np=0;ind=0}{ if($3~ /Indeterminate/) {ind++;}else if($3~ /NON-SECRETED/) {if($1>92)fn++;}else if($3~ /SECRETED/){if($1<=92)fp++;}else if($3~ /Noise/){if($1<=92)nn++; else np++}}END{ printf "\t%d\t%.2f\t%d\t%.2f\t%.2f", fp, (92-nn-fp)/(92-nn), fn, (14-np-fn)/(14-np), (92-nn-fp+14-np-fn)/(106-nn-np);}' $run-teste1.result >> group$1.result
+                            printf "." 
+			    
+			    rm  $run-*.arff  $run-model.bin  $run-teste?.result
+			else
+			    echo no mask file
+			fi
+		    else
+			echo model.bin missing 
+		    fi
+		else
+		    echo training missing
+		fi
+	else
+	    echo training missing
+	fi
+    done
+else
+    echo invalid parameters $1 $2 
+fi
