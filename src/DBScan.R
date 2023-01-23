27f2ad20bedfa70d823ee4f1ca81a40b347f9dae
Anderson Santos <santosardr@gmail.com>
2023-01-23 08:24:08 -0300

    Preparing article ...

--- src/DBScan.R
+++ src/DBScan.R
@@ -3,23 +3,33 @@ WPM("load-packages", "optics_dbScan")
 DBScan<-make_Weka_clusterer('weka/clusterers/DBSCAN', c("DBScan", "Weka_clusterers"), package = "optics_dbScan")
 data<-read.arff("training.arff")
 
-control<-c( "-E", 1.1, "-M", 6, "-A", "weka.core.EuclideanDistance", "--", "-R", "first-last" );
+control<-c( "-E", 0.7, "-M", 6, "-A", "weka.core.EuclideanDistance", "--", "-R", "first-last" );
 fullresult <- DBScan(data, control);
 table(predict(fullresult), data$class);
 result<-predict(fullresult);
 limP=141;
 limN=length(result);
 pos<-result[1:limP];neg<-result[(limP+1):limN];
+printf <- function(...) cat(sprintf(...))
 cat(sprintf("\nFalse Negative\n"));neg[which(is.na(neg))];
 cat(sprintf("\nFalse Positive\n"));pos[which(is.na(pos))];
 length(as.numeric(labels(neg[which(is.na(neg))])));
 length(as.numeric(labels(pos[which(is.na(pos))])))
 
-printf <- function(...) cat(sprintf(...))
+#for ( c in 0:3) { lista=as.numeric(labels(neg[which(neg==c)]))+144;for(item in lista){ printf("%dd;",  item) }}
+
 lista=as.numeric(labels(neg[which(is.na(neg))]))+144
-for(item in lista){ printf("%dd;",  item) }
-
-lista=as.numeric(labels(neg[which(neg==1)]))+144;for(item in lista){ printf("%d ",  item) }
-i=0; preditos=(  ); for indice in ${preditos[@]}; do indice=$[$indice+$i]; sed -i "$[indice]d" filtered.arff; i=$[$i-1];done
-
+sink("filter3.result");
+count=1;
+upcount=0;
+for(item in lista){
+	 count=count+1
+	 if (!count%%10000) {
+	    upcount= upcount + 10000;
+	    printf("%dd\n",  item-upcount)
+	 }else{
+		printf("%dd;",  item-upcount)
+	}
+}
+sink();
 
