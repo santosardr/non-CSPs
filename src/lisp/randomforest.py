0d9b3faa3cfce7a0e2cc062453e9b5226a046bde
Anderson Santos <santosardr@ufu.br>
2023-09-30 17:33:44 -0300

    Standardizing like the Lisp version

--- src/lisp/randomforest.py
+++ src/lisp/randomforest.py
@@ -1,107 +1,139 @@
+#!/usr/bin/python3
+import os
 import sys
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import confusion_matrix, roc_curve, auc
 import matplotlib.pyplot as plt
 
-# Read the training ARFF file
-train_file = sys.argv[1]
-train_data = pd.read_csv(train_file, comment='@', header=None)
+def train_predict(train_file, test_file, class_border):
 
-# Separate the features (attributes) and the labels
-X_train = train_data.iloc[:, :-1]
-y_train = train_data.iloc[:, -1]
+    # Read the training ARFF file
+    train_data = pd.read_csv(train_file, comment='@', header=None)
 
-# Read the testing ARFF file
-test_file = sys.argv[2]
-test_data = pd.read_csv(test_file, comment='@', header=None)
+    # Separate the features (attributes) and the labels
+    X_train = train_data.iloc[:, :-1]
+    y_train = train_data.iloc[:, -1]
 
-# Separate the features (attributes) for prediction
-X_test = test_data.iloc[:, :-1]
+    # Read the testing ARFF file
+    test_data = pd.read_csv(test_file, comment='@', header=None)
 
-# Read the limits for NON-SECRETED and SECRETED
-non_secreted_limit = int(sys.argv[3])
-secreted_limit = int(sys.argv[4])
+    # Separate the features (attributes) for prediction
+    X_test = test_data.iloc[:, :-1]
 
-# Create and train the random forest model
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
+    # Create and train the random forest model
+    model = RandomForestClassifier()
+    model.fit(X_train, y_train)
 
-# Predict the labels for the testing data
-y_pred = model.predict(X_test)
+    # Predict the labels for the testing data
+    y_pred = model.predict(X_test)
 
-# Initialize the confusion matrix
-confusion = [[0, 0], [0, 0]]
+    # Initialize the confusion matrix
+    confusion = [[0, 0], [0, 0]]
 
-# Compare the predicted labels with the actual labels and increment the confusion matrix accordingly
-for i in range(len(y_pred)):
-    if i < non_secreted_limit:
-        if y_pred[i] == 'NON-SECRETED':
-            confusion[0][0] += 1  # True Negative (NON-SECRETED predicted correctly)
+    # Compare the predicted labels with the actual labels and increment the confusion matrix accordingly
+    for i in range(len(y_pred)):
+        if i < class_border:
+            if y_pred[i] == 'NON-SECRETED':
+                confusion[0][0] += 1  # True Negative (NEGATIVE predicted correctly)
+            else:
+                confusion[0][1] += 1  # False Positive (SECRETED predicted incorrectly)
         else:
-            confusion[0][1] += 1  # False Positive (SECRETED predicted incorrectly)
+            if y_pred[i] == 'SECRETED':
+                confusion[1][1] += 1  # True Positive (SECRETED predicted correctly)
+            else:
+                confusion[1][0] += 1  # False Negative (NEGATIVE predicted incorrectly)
+
+    # Extract the true positives (TP), false negatives (FN), true negatives (TN), and false positives (FP) from the confusion matrix
+    tp = confusion[1][1]
+    fn = confusion[1][0]
+    tn = confusion[0][0]
+    fp = confusion[0][1]
+
+    # Calculate the sensitivity and specificity
+    if tp + fn == 0:
+        sensitivity = 0
     else:
-        if y_pred[i] == 'SECRETED':
-            confusion[1][1] += 1  # True Positive (SECRETED predicted correctly)
-        else:
-            confusion[1][0] += 1  # False Negative (NON-SECRETED predicted incorrectly)
+        sensitivity = tp / (tp + fn)
 
-# Extract the true positives (TP), false negatives (FN), true negatives (TN), and false positives (FP) from the confusion matrix
-tp = confusion[1][1]
-fn = confusion[1][0]
-tn = confusion[0][0]
-fp = confusion[0][1]
+    specificity = tn / (tn + fp)
 
-# Calculate the sensitivity and specificity
-if tp + fn == 0:
-    sensitivity = 0
-else:
-    sensitivity = tp / (tp + fn)
+    # Calculate the accuracy
+    accuracy = (tp + tn) / (tp + tn + fp + fn)
 
-specificity = tn / (tn + fp)
+    # Print the confusion matrix
+    print(f"\n\tConfusion Matrix:")
+    print(f"\ta\tb\t<-- classified as")
+    print(f"\t{confusion[0][0]}\t{confusion[0][1]}\ta = NEGATIVE")
+    print(f"\t{confusion[1][0]}\t{confusion[1][1]}\tb = POSITIVE\n")
 
-# Calculate the accuracy
-accuracy = (tp + tn) / (tp + tn + fp + fn)
+    # Print the variables involved for debugging
+    print(f"True Negatives:\t{tn}")
+    print(f"False Positives:\t{fp}")
+    print(f"True Positives:\t{tp}")
+    print(f"False Negatives:\t{fn}\n")
 
-# Print the confusion matrix
-print(f"\n\tConfusion Matrix:")
-print(f"\ta\tb\t<-- classified as")
-print(f"\t{confusion[0][0]}\t{confusion[0][1]}\ta = NON-SECRETED")
-print(f"\t{confusion[1][0]}\t{confusion[1][1]}\tb = SECRETED\n")
+    # Print the sensitivity, specificity, and accuracy
+    print(f"Sensitivity: {sensitivity}")
+    print(f"Specificity: {specificity}")
+    print(f"Accuracy: {accuracy}\n")
 
-# Print the variables involved for debugging
-print(f"True Positives:\t{tp}")
-print(f"False Negatives:\t{fn}")
-print(f"True Negatives:\t{tn}")
-print(f"False Positives:\t{fp}\n")
+    # Set the true labels for the testing data based on the limits
+    y_test = [0] * class_border + [1] * (len(X_test) - class_border)
 
-# Print the sensitivity, specificity, and accuracy
-print(f"Sensitivity: {sensitivity}")
-print(f"Specificity: {specificity}")
-print(f"Accuracy: {accuracy}\n")
+    # Predict the probability of the positive class for the testing data
+    y_pred_prob = model.predict_proba(X_test)[:, 1]
 
-# Rest of the code...
+    # Calculate the false positive rate (FPR), true positive rate (TPR), and thresholds for the ROC curve
+    fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
 
-# Set the true labels for the testing data based on the limits
-y_test = [0] * non_secreted_limit + [1] * (len(X_test) - non_secreted_limit)
+    # Calculate the area under the ROC curve (AUC)
+    roc_auc = auc(fpr, tpr)
 
-# Predict the probability of the positive class for the testing data
-y_pred_prob = model.predict_proba(X_test)[:, 1]
+    # Plot the ROC curve
+    plt.figure()
+    plt.plot(fpr, tpr, label='ROC curve (AUC = %0.2f)' % roc_auc)
+    plt.plot([0, 1], [0, 1], 'k--')  # Diagonal line representing random guessing
+    plt.xlim([0.0, 1.0])
+    plt.ylim([0.0, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver Operating Characteristic')
+    plt.legend(loc='lower right')
+    plt.show()
 
-# Calculate the false positive rate (FPR), true positive rate (TPR), and thresholds for the ROC curve
-fpr, tpr, thresholds = roc_curve(y_test, y_pred_prob)
 
-# Calculate the area under the ROC curve (AUC)
-roc_auc = auc(fpr, tpr)
+def main():
+    args = sys.argv
+    arg_count = len(args)
+    test_file = None
+    class_border = None
+    train_file = None
 
-# Plot the ROC curve
-plt.figure()
-plt.plot(fpr, tpr, label='ROC curve (AUC = %0.2f)' % roc_auc)
-plt.plot([0, 1], [0, 1], 'k--')  # Diagonal line representing random guessing
-plt.xlim([0.0, 1.0])
-plt.ylim([0.0, 1.05])
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic')
-plt.legend(loc='lower right')
-plt.show()
+    if arg_count < 4:
+        print("Error: Invalid parameters.")
+        print("The first argument must be a test file, the second argument must be an integer, and the third the training file.")
+        print("Example: ./randomforest.py validation1.arff 94 src/myids-filter5-89-93-90-a.arff")
+        sys.exit()
+
+    test_file = args[1]
+    class_border = int(args[2])
+    train_file = args[3]
+    print(f"Test file: {test_file}")
+    print(f"Test file class border at: {class_border}")
+    print(f"Training file {train_file}")
+
+    if test_file and train_file:
+        if not (os.path.isfile(test_file) and os.path.isfile(train_file)):
+            print("Error: Invalid parameters: check if the path is a valid file.")
+            sys.exit()
+        
+        if class_border is None:
+            print("Error: Invalid parameters: check class-border parameter.")
+            sys.exit()
+        
+        train_predict(train_file, test_file, class_border)
+    else:
+        print("Error: Invalid files: check for valid test and training files.")
+        sys.exit()
+main()
